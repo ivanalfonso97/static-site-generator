a94bfcf5c9fd4b06440176d416f859f4c0b35840
ivanalfonso97 <ivanalfonso.chan@gmail.com>
2025-03-28 16:37:07 +0800

    create link and image extractors

--- src/node_functions.py
+++ src/node_functions.py
@@ -1,5 +1,6 @@
 from textnode import TextType, TextNode
 from htmlnode import LeafNode
+import re
 
 def text_node_to_html_node(text_node):
     match (text_node.text_type):
@@ -21,10 +22,21 @@ def text_node_to_html_node(text_node):
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
-        start, inline, end = node.text.split(delimiter)
-        new_nodes += [
-            TextNode(start, TextType.TEXT),
-            TextNode(inline, text_type),
-            TextNode(end, TextType.TEXT)
-        ]
+        if node.text_type != TextType.TEXT:
+            new_nodes += [node]
+        else:
+            if delimiter not in node.text:
+                raise Exception("Delimiter not found in text")
+            start, inline, end = node.text.split(delimiter)
+            new_nodes += [
+                TextNode(start, TextType.TEXT),
+                TextNode(inline, text_type),
+                TextNode(end, TextType.TEXT)
+            ]
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,12 @@
 import unittest
 
 from textnode import TextNode, TextType
-from node_functions import text_node_to_html_node, split_nodes_delimiter
+from node_functions import (
+    text_node_to_html_node,
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links
+)
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -34,6 +39,26 @@ class TestTextNode(unittest.TestCase):
             TextNode("code block", TextType.CODE),
             TextNode(" word", TextType.TEXT),
         ])
+
+    def test_split_error(self):
+        node = TextNode("This is text with a code block word", TextType.TEXT)
+
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter([node], "`", TextType.CODE)
+
+        self.assertEqual(str(context.exception), "Delimiter not found in text")
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
 
 if __name__ == "__main__":
     unittest.main()
