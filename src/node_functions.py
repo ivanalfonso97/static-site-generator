2da78d9dbfb53af49ed0bfca6c47885d25c44dea
ivanalfonso97 <ivanalfonso.chan@gmail.com>
2025-03-28 17:31:04 +0800

    create link and image splitters

--- src/node_functions.py
+++ src/node_functions.py
@@ -35,6 +35,54 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             ]
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        current_text = node.text
+        found_images = extract_markdown_images(node.text)
+
+        if len(found_images) == 0:
+            return [node]
+
+        for alt_text, url in found_images:
+            markdown = f"![{alt_text}]({url})"
+            sections = current_text.split(markdown, maxsplit=1)
+
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+            current_text = sections[1]
+
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        current_text = node.text
+        found_links = extract_markdown_links(node.text)
+
+        if len(found_links) == 0:
+            return [node]
+
+        for link_text, url in found_links:
+            markdown = f"[{link_text}]({url})"
+            sections = current_text.split(markdown, maxsplit=1)
+
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(link_text, TextType.LINK, url))
+            current_text = sections[1]
+
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+    return new_nodes
+
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
